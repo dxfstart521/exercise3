57a18ca48752cb74970414173bdc8360901458cd
dingxiaofei <dingxiaofei@didichuxing.com>
2018-01-29 14:44:19 +0800

    update karma

--- karma.conf.js
+++ karma.conf.js
@@ -57,7 +57,7 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome','ChromeHeadlessNoSandbox'],
+        browsers: ['ChromeHeadlessNoSandbox'],
 
         // travis cli 使用 chrome
         customLaunchers: {
